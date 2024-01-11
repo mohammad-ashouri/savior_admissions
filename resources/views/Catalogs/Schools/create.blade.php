9b6d27392b28c9ad105b5d981c0018a6ca78e392
Mohammad Ashouri <exploiter0253@gmail.com>
2024-01-11 23:13:29 +0330

    Fix school catalog text bugs

--- resources/views/Catalogs/Schools/create.blade.php
+++ resources/views/Catalogs/Schools/create.blade.php
@@ -41,7 +41,7 @@
                             <div>
                                 <label for="gender"
                                        class="block mb-2  font-bold text-gray-900 dark:text-white">School
-                                    name</label>
+                                    type</label>
                                 <select type="text" id="gender" name="gender"
                                         class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                         title="Select gender" required>
