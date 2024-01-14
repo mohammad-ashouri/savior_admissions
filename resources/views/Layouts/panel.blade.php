acc89d32b67250a6f608021df4dc6ea32b8ab613
Mohammad Ashouri <exploiter0253@gmail.com>
2024-01-14 14:08:28 +0330

    Add branch info menus

--- resources/views/Layouts/panel.blade.php
+++ resources/views/Layouts/panel.blade.php
@@ -142,6 +142,57 @@
                     </a>
                 </li>
                 @if($myInfo->hasRole('SuperAdmin'))
+
+                    <li>
+                        <button type="button"
+                                class="flex items-center w-full p-2 text-base text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
+                                aria-controls="dropdown-example" data-collapse-toggle="dropdown-example">
+                            <i class="nav-icon la la-landmark" style="font-size: 24px"></i>
+                            <span class="flex-1 ml-4 text-left whitespace-nowrap">Branch Info</span>
+                            <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                                 viewBox="0 0 10 6">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                      stroke-width="2" d="m1 1 4 4 4-4"/>
+                            </svg>
+                        </button>
+                        <ul id="dropdown-example" class="hidden py-2 space-y-2">
+                            <li>
+                                <a href="/Classes"
+                                   class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                                    <span class="menulist"><i class="nav-icon la la-chalkboard-teacher" style="font-size: 24px"></i>
+                                        Classes</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="/Applications"
+                                   class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                                    <span class="menulist"><i class="nav-icon la la-paste" style="font-size: 24px"></i>
+                                        Applications</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="/Fees"
+                                   class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                                    <span class="menulist"><i class="nav-icon la la-money-bill-wave-alt" style="font-size: 24px"></i>
+                                        Fees</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="/Interviews"
+                                   class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                                    <span class="menulist"><i class="nav-icon la la-book-reader" style="font-size: 24px"></i>
+                                        Interviews</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="/Students"
+                                   class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                                    <span class="menulist"><i class="nav-icon la la-user-graduate" style="font-size: 24px"></i>
+                                        Students</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
                     <li>
                         <button type="button"
                                 class="flex items-center w-full p-2 text-base text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
