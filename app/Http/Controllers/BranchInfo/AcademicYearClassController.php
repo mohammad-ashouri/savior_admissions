3843196afed15eea0c296e83fb05f1210886313c
Mohammad Ashouri <exploiter0253@gmail.com>
2024-01-22 23:31:59 +0330

    Create academic year classes frontend fixed

--- app/Http/Controllers/BranchInfo/AcademicYearClassController.php
+++ app/Http/Controllers/BranchInfo/AcademicYearClassController.php
@@ -4,6 +4,11 @@ namespace App\Http\Controllers\BranchInfo;
 
 use App\Http\Controllers\Controller;
 use App\Models\Branch\AcademicYearClass;
+use App\Models\Catalogs\AcademicYear;
+use App\Models\Catalogs\EducationType;
+use App\Models\Catalogs\Level;
+use App\Models\Gender;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AcademicYearClassController extends Controller
@@ -23,4 +28,18 @@ class AcademicYearClassController extends Controller
         $academicYearClasses = AcademicYearClass::with('academicYearInfo')->with('levelInfo')->with('educationTypeInfo')->with('educationGenderInfo')->orderBy('id', 'desc')->paginate(10);
         return view('BranchInfo.AcademicYearClasses.index', compact('academicYearClasses'));
     }
+
+    public function create()
+    {
+        $me=User::find(session('id'));
+        if ($me->hasRole('Super Admin')) {
+            $academicYears = AcademicYear::where('status', 1)->get();
+            $levels=Level::where('status',1)->get();
+            $educationTypes=EducationType::where('status',1)->get();
+            $educationGenders=Gender::get();
+        }
+        return view('BranchInfo.AcademicYearClasses.create', compact('academicYears','levels','educationTypes','educationGenders'));
+    }
+
+
 }
