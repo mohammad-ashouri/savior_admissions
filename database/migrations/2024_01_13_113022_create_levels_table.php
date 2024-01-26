aaeac07a938258c80b0ec69528810e925dc6d7aa
Mohammad Ashouri <exploiter0253@gmail.com>
2024-01-26 13:33:15 +0330

    Remove SoftDeletes from levels table

--- database/migrations/2024_01_13_113022_create_levels_table.php
+++ database/migrations/2024_01_13_113022_create_levels_table.php
@@ -17,7 +17,6 @@ return new class extends Migration
             $table->string('name');
             $table->boolean('status')->default(1);
             $table->timestamps();
-            $table->softDeletes();
         });
         $query="insert into levels (name) values ('Kindergarten 1'),('Kindergarten 2'),('Grade 1'),('Grade 2'),('Grade 3'),('Grade 4'),('Grade 5'),('Grade 6'),('Grade 7'),('Grade 8'),('Grade 9'),('Grade 10'),('Grade 11'),('Grade 12')";
         DB::statement($query);
