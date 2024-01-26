b69f2b2e3cdab7f77f18676f930e57cfd7f77f53
Mohammad Ashouri <exploiter0253@gmail.com>
2024-01-26 19:34:46 +0330

    Add countries migration+model+SQL data

---
+++ database/migrations/2023_01_26_190320_create_countries_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('countries', function (Blueprint $table) {
+            $table->id();
+            $table->integer('num_code');
+            $table->string('alpha_2_code');
+            $table->string('alpha_3_code');
+            $table->string('en_short_name');
+            $table->string('nationality');
+        });
+        DB::unprepared(file_get_contents('database/migrations/countries.sql'));
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('countries');
+    }
+};
