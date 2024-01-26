e007aeb349601ce3494b049e703a1156ac879c4b
Mohammad Ashouri <exploiter0253@gmail.com>
2024-01-26 13:33:48 +0330

    Remove SoftDeletes from levels model

--- app/Models/Catalogs/Level.php
+++ app/Models/Catalogs/Level.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Level extends Model
 {
-    use HasFactory,SoftDeletes;
+    use HasFactory;
     protected $table = 'levels';
     protected $fillable = [
         'name',
