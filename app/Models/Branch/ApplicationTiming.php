3cf11cf43c949ff3d9c28e9c88b25a9319610287
Me Home <exploiter0253@gmail.com>
2024-01-31 11:40:49 +0330

    Application timing model fixed

--- app/Models/Branch/ApplicationTiming.php
+++ app/Models/Branch/ApplicationTiming.php
@@ -2,10 +2,39 @@
 
 namespace App\Models\Branch;
 
+use App\Models\Catalogs\AcademicYear;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class ApplicationTiming extends Model
 {
     use HasFactory;
+
+    protected $table = 'application_timings';
+
+    protected $fillable = [
+        'academic_year',
+        'students_application_type',
+        'start_date',
+        'start_time',
+        'end_date',
+        'end_time',
+        'interview_time',
+        'delay_between_reserve',
+        'interviewers',
+        'fee',
+    ];
+
+    protected $casts = [
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime',
+        'deleted_at' => 'datetime',
+    ];
+
+    public function academicYearInfo()
+    {
+        return $this->belongsTo(AcademicYear::class, 'academic_year', 'id');
+    }
+
+
 }
