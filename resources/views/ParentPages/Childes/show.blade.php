df467a66f57c710b253a9a5177e1d8a657a2c97a
Me Home <exploiter0253@gmail.com>
2024-02-12 20:16:53 +0330

    Fix showing alert if extra informations is empty in show child informations

--- resources/views/ParentPages/Childes/show.blade.php
+++ resources/views/ParentPages/Childes/show.blade.php
@@ -56,50 +56,71 @@
             <div class="grid grid-cols-2 gap-4 mb-4">
                 <div class="lg:col-span-2 col-span-2 ">
                     <div class="general-info bg-white dark:bg-gray-800 dark:text-white p-8 rounded-lg mb-4">
-                        <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-                            <thead
-                                class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                            <tr>
-                                <th scope="col" class="p-4">
-                                    <div class="flex items-center">
-                                        #
-                                    </div>
-                                </th>
-                                <th scope="col" class="p-4">
-                                    <div class="flex items-center">
-                                        Title
-                                    </div>
-                                </th>
-                                <th scope="col" class="p-4">
-                                    <div class="flex items-center">
-                                        Description
-                                    </div>
-                                </th>
-                            </tr>
-                            </thead>
-                            <tbody>
-                            @foreach($studentInformations->extraInformations as $extraInformation)
-                                <tr
-                                    class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                                    <td class="w-4 p-4">
+
+                        @if(isset($studentInformations->extraInformations) and !$studentInformations->extraInformations->isEmpty())
+                            <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                                <thead
+                                    class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                                <tr>
+                                    <th scope="col" class="p-4">
                                         <div class="flex items-center">
-                                            {{ $loop->iteration }}
+                                            #
                                         </div>
-                                    </td>
-                                    <td class="w-4 p-4">
+                                    </th>
+                                    <th scope="col" class="p-4">
                                         <div class="flex items-center">
-                                            {{ $extraInformation->name }}
+                                            Title
                                         </div>
-                                    </td>
-                                    <td class="w-4 p-4">
+                                    </th>
+                                    <th scope="col" class="p-4">
                                         <div class="flex items-center">
-                                            {{ $extraInformation->description }}
+                                            Description
                                         </div>
-                                    </td>
+                                    </th>
                                 </tr>
-                            </tbody>
-                            @endforeach
-                        </table>
+                                </thead>
+                                <tbody>
+                                @foreach($studentInformations->extraInformations as $extraInformation)
+                                    <tr
+                                        class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                                        <td class="w-4 p-4">
+                                            <div class="flex items-center">
+                                                {{ $loop->iteration }}
+                                            </div>
+                                        </td>
+                                        <td class="w-4 p-4">
+                                            <div class="flex items-center">
+                                                {{ $extraInformation->name }}
+                                            </div>
+                                        </td>
+                                        <td class="w-4 p-4">
+                                            <div class="flex items-center">
+                                                {{ $extraInformation->description }}
+                                            </div>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                                @endforeach
+                            </table>
+                        @else
+                            <div
+                                class="bg-teal-100 border-t-4 border-teal-500 rounded-b text-teal-900 px-4 py-3 shadow-md"
+                                role="alert">
+                                <div class="flex">
+                                    <div class="py-1">
+                                        <svg class="fill-current h-6 w-6 text-teal-500 mr-4"
+                                             xmlns="http://www.w3.org/2000/svg"
+                                             viewBox="0 0 20 20">
+                                            <path
+                                                d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zm12.73-1.41A8 8 0 1 0 4.34 4.34a8 8 0 0 0 11.32 11.32zM9 11V9h2v6H9v-4zm0-6h2v2H9V5z"/>
+                                        </svg>
+                                    </div>
+                                    <div>
+                                        <p class="font-bold">There is not any extra informations!</p>
+                                    </div>
+                                </div>
+                            </div>
+                        @endif
                     </div>
                 </div>
             </div>
