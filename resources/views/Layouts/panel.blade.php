2d2b93242499e4fa805a36cf8499d484a914fd01
Me Home <exploiter0253@gmail.com>
2024-02-16 23:07:08 +0330

    Fix user menu access

--- resources/views/Layouts/panel.blade.php
+++ resources/views/Layouts/panel.blade.php
@@ -160,7 +160,7 @@
                     </a>
                 </li>
             @endcan
-            @can('branch-info-menu-access')
+            @can('users-menu-access')
                 <li>
                     <a href="/users"
                        class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
