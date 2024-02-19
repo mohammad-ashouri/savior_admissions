ee6d697302656dea08c671839a51ab51e62d0dd6
Me Home <exploiter0253@gmail.com>
2024-02-19 21:39:33 +0330

    Submit interview fixed + FeeController.php removed

--- app/Http/Controllers/BranchInfo/InterviewController.php
+++ app/Http/Controllers/BranchInfo/InterviewController.php
@@ -6,22 +6,24 @@ use App\Http\Controllers\Controller;
 use App\Models\Branch\ApplicationReservation;
 use App\Models\Branch\Applications;
 use App\Models\Branch\ApplicationTiming;
+use App\Models\Branch\Interview;
 use App\Models\Catalogs\AcademicYear;
 use App\Models\StudentInformation;
 use App\Models\User;
 use App\Models\UserAccessInformation;
+use Illuminate\Http\Request;
 
 class InterviewController extends Controller
 {
-        public function __construct()
-        {
-            $this->middleware('permission:interview-list', ['only' => ['index']]);
-            $this->middleware('permission:interview-set', ['only' => ['set']]);
-            $this->middleware('permission:interview-edit', ['only' => ['edit', 'update']]);
-            $this->middleware('permission:interview-delete', ['only' => ['destroy']]);
-            $this->middleware('permission:interview-search', ['only' => ['search']]);
-            $this->middleware('permission:interview-show', ['only' => ['show']]);
-        }
+    public function __construct()
+    {
+        $this->middleware('permission:interview-list', ['only' => ['index']]);
+        $this->middleware('permission:interview-set', ['only' => ['SetInterview']]);
+        $this->middleware('permission:interview-edit', ['only' => ['edit', 'update']]);
+        $this->middleware('permission:interview-delete', ['only' => ['destroy']]);
+        $this->middleware('permission:interview-search', ['only' => ['search']]);
+        $this->middleware('permission:interview-show', ['only' => ['show', 'GetInterviewForm']]);
+    }
 
     public function index()
     {
@@ -33,18 +35,18 @@ class InterviewController extends Controller
                 ->with('reservatoreInfo')
                 ->with('applicationInvoiceInfo')
                 ->whereIn('student_id', $myStudents)
-                ->orderBy('date','desc')
-                ->orderBy('ends_to','desc')
-                ->orderBy('start_from','desc')
+                ->orderBy('date', 'desc')
+                ->orderBy('ends_to', 'desc')
+                ->orderBy('start_from', 'desc')
                 ->paginate(30);
         } elseif ($me->hasRole('Super Admin')) {
             $interviews = Applications::with('applicationTimingInfo')
                 ->with('interviewerInfo')
                 ->where('reserved', 1)
                 ->where('reserved', 1)
-                ->orderBy('date','desc')
-                ->orderBy('ends_to','desc')
-                ->orderBy('start_from','desc')
+                ->orderBy('date', 'desc')
+                ->orderBy('ends_to', 'desc')
+                ->orderBy('start_from', 'desc')
                 ->paginate(30);
         } elseif ($me->hasRole('Principal') or $me->hasRole('Admissions Officer')) {
             // Convert accesses to arrays and remove duplicates
@@ -65,9 +67,9 @@ class InterviewController extends Controller
                 ->where('reserved', 1)
                 ->whereIn('application_timing_id', $applicationTimings)
                 ->where('reserved', 1)
-                ->orderBy('date','desc')
-                ->orderBy('ends_to','desc')
-                ->orderBy('start_from','desc')
+                ->orderBy('date', 'desc')
+                ->orderBy('ends_to', 'desc')
+                ->orderBy('start_from', 'desc')
                 ->paginate(30);
 
         } elseif ($me->hasRole('Interviewer')) {
@@ -77,9 +79,9 @@ class InterviewController extends Controller
                 ->where('reserved', 1)
                 ->where('interviewer', $me->id)
                 ->where('Interviewed', 0)
-                ->orderBy('date','desc')
-                ->orderBy('ends_to','desc')
-                ->orderBy('start_from','desc')
+                ->orderBy('date', 'desc')
+                ->orderBy('ends_to', 'desc')
+                ->orderBy('start_from', 'desc')
                 ->paginate(30);
         }
 
@@ -95,26 +97,15 @@ class InterviewController extends Controller
     {
         $me = User::find(session('id'));
         $interview = [];
-        if ($me->hasRole('Parent(Father)') or $me->hasRole('Parent(Mother)')) {
-            $myStudents = StudentInformation::where('guardian', $me->id)->pluck('student_id')->toArray();
-            $interview = ApplicationReservation::with('studentInfo')
-                ->with('reservatoreInfo')
-                ->with('applicationInvoiceInfo')
-                ->whereIn('student_id', $myStudents)
-                ->where('id', $id)
-                ->orderBy('date','desc')
-                ->orderBy('ends_to','desc')
-                ->orderBy('start_from','desc')
-                ->first();
-        } elseif ($me->hasRole('Super Admin')) {
+        if ($me->hasRole('Super Admin')) {
             $interview = Applications::with('applicationTimingInfo')
                 ->with('interviewerInfo')
                 ->where('reserved', 1)
                 ->where('reserved', 1)
                 ->where('id', $id)
-                ->orderBy('date','desc')
-                ->orderBy('ends_to','desc')
-                ->orderBy('start_from','desc')
+                ->orderBy('date', 'desc')
+                ->orderBy('ends_to', 'desc')
+                ->orderBy('start_from', 'desc')
                 ->first();
         } elseif ($me->hasRole('Principal') or $me->hasRole('Admissions Officer')) {
             // Convert accesses to arrays and remove duplicates
@@ -136,9 +127,9 @@ class InterviewController extends Controller
                 ->whereIn('application_timing_id', $applicationTimings)
                 ->where('reserved', 1)
                 ->where('id', $id)
-                ->orderBy('date','desc')
-                ->orderBy('ends_to','desc')
-                ->orderBy('start_from','desc')
+                ->orderBy('date', 'desc')
+                ->orderBy('ends_to', 'desc')
+                ->orderBy('start_from', 'desc')
                 ->first();
 
         } elseif ($me->hasRole('Interviewer')) {
@@ -149,9 +140,9 @@ class InterviewController extends Controller
                 ->where('interviewer', $me->id)
                 ->where('Interviewed', 0)
                 ->where('id', $id)
-                ->orderBy('date','desc')
-                ->orderBy('ends_to','desc')
-                ->orderBy('start_from','desc')
+                ->orderBy('date', 'desc')
+                ->orderBy('ends_to', 'desc')
+                ->orderBy('start_from', 'desc')
                 ->first();
         }
         if (empty($interview)) {
@@ -159,5 +150,80 @@ class InterviewController extends Controller
         }
 
         return view('BranchInfo.Interviews.set', compact('interview'));
+    }
+
+    public function SetInterview(Request $request)
+    {
+        $me = User::find(session('id'));
+        $interview = [];
+        if ($me->hasRole('Super Admin')) {
+            $interview = Applications::with('applicationTimingInfo')
+                ->with('interviewerInfo')
+                ->where('reserved', 1)
+                ->where('reserved', 1)
+                ->where('id', $request->application_id)
+                ->orderBy('date', 'desc')
+                ->orderBy('ends_to', 'desc')
+                ->orderBy('start_from', 'desc')
+                ->first();
+        } elseif ($me->hasRole('Principal') or $me->hasRole('Admissions Officer')) {
+            // Convert accesses to arrays and remove duplicates
+            $myAllAccesses = UserAccessInformation::where('user_id', $me->id)->first();
+            $principalAccess = explode('|', $myAllAccesses->principal);
+            $financialManagerAccess = explode('|', $myAllAccesses->financial_manager);
+            $filteredArray = array_filter(array_unique(array_merge($principalAccess, $financialManagerAccess)));
+
+            // Finding academic years with status 1 in the specified schools
+            $academicYears = AcademicYear::where('status', 1)->whereIn('school_id', $filteredArray)->pluck('id')->toArray();
+
+            // Finding application timings based on academic years
+            $applicationTimings = ApplicationTiming::whereIn('academic_year', $academicYears)->pluck('id')->toArray();
+
+            // Finding applications related to the application timings
+            $interview = Applications::with('applicationTimingInfo')
+                ->with('interviewerInfo')
+                ->where('reserved', 1)
+                ->whereIn('application_timing_id', $applicationTimings)
+                ->where('reserved', 1)
+                ->where('id', $request->application_id)
+                ->orderBy('date', 'desc')
+                ->orderBy('ends_to', 'desc')
+                ->orderBy('start_from', 'desc')
+                ->first();
+
+        } elseif ($me->hasRole('Interviewer')) {
+            $interview = Applications::with('applicationTimingInfo')
+                ->with('interviewerInfo')
+                ->with('reservationInfo')
+                ->where('reserved', 1)
+                ->where('interviewer', $me->id)
+                ->where('Interviewed', 0)
+                ->where('id', $request->application_id)
+                ->orderBy('date', 'desc')
+                ->orderBy('ends_to', 'desc')
+                ->orderBy('start_from', 'desc')
+                ->first();
+        }
+        if (empty($interview)) {
+            abort(403);
+        }
+
+        $interview = new Interview();
+        $interview->application_id = $request->application_id;
+        $interview->interview_form = json_encode($request->all(), true);
+        if ($interview->save()) {
+            $application = Applications::find($request->application_id);
+            $application->interviewed = 1;
+            if ($application->save()) {
+                return redirect()->route('Interviews.index')
+                    ->with('success', 'The interview was successfully recorded');
+            }
+
+            return redirect()->route('Interviews.index')
+                ->withErrors(['errors' => 'Recording the interview failed!']);
+        }
+
+        return redirect()->route('Interviews.index')
+            ->withErrors(['errors' => 'Recording the interview failed!']);
     }
 }
