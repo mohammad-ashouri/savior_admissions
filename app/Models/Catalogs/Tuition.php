10f74da1fb63956d6fdecdcdb21f408a568fc60e
Me Home <exploiter0253@gmail.com>
2024-02-22 17:04:00 +0330

    Add tuition and tuition invoices migration+model

---
+++ app/Models/Catalogs/Tuition.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Catalogs;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Tuition extends Model
+{
+    use HasFactory;
+}
