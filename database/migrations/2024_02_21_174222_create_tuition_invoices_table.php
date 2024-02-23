bc652326434fd9fab89db8d663a26787a819d6c7
Me Home <exploiter0253@gmail.com>
2024-02-23 18:01:06 +0330

    Fix tuition_invoices migration table name

--- database/migrations/2024_02_21_174222_create_tuition_invoices_table.php
+++ database/migrations/2024_02_21_174222_create_tuition_invoices_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('tuitions', function (Blueprint $table) {
+        Schema::create('tuition_invoices', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
         });
@@ -22,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('tuitions');
+        Schema::dropIfExists('tuition_invoices');
     }
 };
