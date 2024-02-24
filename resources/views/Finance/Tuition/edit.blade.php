7dcba872c655c3276147367d03f78c4dd525238c
Me Home <exploiter0253@gmail.com>
2024-02-24 16:15:00 +0330

    Add Rials div for tuition fee input

--- resources/views/Finance/Tuition/edit.blade.php
+++ resources/views/Finance/Tuition/edit.blade.php
@@ -47,7 +47,7 @@
                                             </th>
                                             <th scope="col" class="p-4">
                                                 <div class=" items-center text-center">
-                                                    Price
+                                                    Fee
                                                 </div>
                                             </th>
                                             <th scope="col" class="p-4">
@@ -67,12 +67,18 @@
                                                     </td>
                                                     <td class="p-4">
                                                         @if($tuition->tuitionInfo->academicYearInfo->status==1)
-                                                            <input type="number"
-                                                                   value="{{$tuition->price}}"
-                                                                   data-tuition-id="{{ $tuition->id }}"
-                                                                   placeholder="Enter the tuition fee for {{$tuition->levelInfo->name}} in Rials"
-                                                                   class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 price"
-                                                                   required>
+                                                            <div class="flex justify-between">
+                                                                <input type="number"
+                                                                       value="{{$tuition->price}}"
+                                                                       data-tuition-id="{{ $tuition->id }}"
+                                                                       placeholder="Enter the tuition fee for {{$tuition->levelInfo->name}} in Rials"
+                                                                       class="rounded-s-lg bg-gray-50 border text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-sm border-gray-300 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 price"
+                                                                       required>
+                                                                <span
+                                                                    class="inline-flex items-center px-3 text-sm text-gray-900 bg-gray-200 border rounded-e-0 border-gray-300 rounded-e-md dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600">
+                                                                Rials
+                                                            </span>
+                                                            </div>
                                                         @else
                                                             {{$tuition->price}}
                                                         @endif
