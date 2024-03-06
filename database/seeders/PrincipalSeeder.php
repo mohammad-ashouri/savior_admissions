0f77984d62f0103c3e2853410d8906b3970b58a6
Me Home <exploiter0253@gmail.com>
2024-03-06 22:39:37 +0330

    Comment principal seeder

--- database/seeders/PrincipalSeeder.php
+++ database/seeders/PrincipalSeeder.php
@@ -14,21 +14,21 @@ class PrincipalSeeder extends Seeder
      */
     public function run(): void
     {
-        $user = User::query()->create([
-            'mobile' => '+989029966902',
-            'email' => 'test@savior.ir',
-            'password' => bcrypt(12345678),
-        ]);
-        $generalInformation = GeneralInformation::create(
-            [
-                'user_id' => $user->id,
-                'first_name_fa' => 'رضا',
-                'last_name_fa' => 'قنبری',
-                'first_name_en' => 'Reza',
-                'last_name_en' => 'Ghanbari',
-            ]
-        );
-        $role = Role::where('name', 'Principal')->first();
-        $user->assignRole([$role->id]);
+//        $user = User::query()->create([
+//            'mobile' => '+989029966902',
+//            'email' => 'test@savior.ir',
+//            'password' => bcrypt(12345678),
+//        ]);
+//        $generalInformation = GeneralInformation::create(
+//            [
+//                'user_id' => $user->id,
+//                'first_name_fa' => 'رضا',
+//                'last_name_fa' => 'قنبری',
+//                'first_name_en' => 'Reza',
+//                'last_name_en' => 'Ghanbari',
+//            ]
+//        );
+//        $role = Role::where('name', 'Principal')->first();
+//        $user->assignRole([$role->id]);
     }
 }
