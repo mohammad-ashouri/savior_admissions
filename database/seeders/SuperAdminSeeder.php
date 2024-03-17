d576a4494b43a5ac3c5506fdabc7495dac3f96c4
Me Home <exploiter0253@gmail.com>
2024-03-17 23:52:25 +0330

    Fix main seeder

--- database/seeders/SuperAdminSeeder.php
+++ database/seeders/SuperAdminSeeder.php
@@ -17,39 +17,39 @@ class SuperAdminSeeder extends Seeder
         /**
          * @var User $user
          */
-        $user = User::query()->create([
-            'mobile' => '+989012682581',
-            'email' => 'test@example.com',
-            'password' => bcrypt(12345678),
-        ]);
-        $generalInformation = GeneralInformation::create(
-            [
-                'user_id' => $user->id,
-                'first_name_fa' => 'محمد',
-                'last_name_fa' => 'عاشوری',
-                'first_name_en' => 'Mohammad',
-                'last_name_en' => 'Ashouri',
-            ]
-        );
-        $role = Role::where('name', 'Super Admin')->first();
-        $user->assignRole([$role->id]);
-
-        $user = User::query()->create([
-            'mobile' => '+989029966902',
-            'email' => 'test@savior.ir',
-            'password' => bcrypt(12345678),
-        ]);
-        $generalInformation = GeneralInformation::create(
-            [
-                'user_id' => $user->id,
-                'first_name_fa' => 'رضا',
-                'last_name_fa' => 'قنبری',
-                'first_name_en' => 'Reza',
-                'last_name_en' => 'Ghanbari',
-            ]
-        );
-        $role = Role::where('name', 'Super Admin')->first();
-        $user->assignRole([$role->id]);
+//        $user = User::query()->create([
+//            'mobile' => '+989012682581',
+//            'email' => 'test@example.com',
+//            'password' => bcrypt(12345678),
+//        ]);
+//        $generalInformation = GeneralInformation::create(
+//            [
+//                'user_id' => $user->id,
+//                'first_name_fa' => 'محمد',
+//                'last_name_fa' => 'عاشوری',
+//                'first_name_en' => 'Mohammad',
+//                'last_name_en' => 'Ashouri',
+//            ]
+//        );
+//        $role = Role::where('name', 'Super Admin')->first();
+//        $user->assignRole([$role->id]);
+//
+//        $user = User::query()->create([
+//            'mobile' => '+989029966902',
+//            'email' => 'test@savior.ir',
+//            'password' => bcrypt(12345678),
+//        ]);
+//        $generalInformation = GeneralInformation::create(
+//            [
+//                'user_id' => $user->id,
+//                'first_name_fa' => 'رضا',
+//                'last_name_fa' => 'قنبری',
+//                'first_name_en' => 'Reza',
+//                'last_name_en' => 'Ghanbari',
+//            ]
+//        );
+//        $role = Role::where('name', 'Super Admin')->first();
+//        $user->assignRole([$role->id]);
 
         $user = User::query()->create([
             'mobile' => '+989011111111',
