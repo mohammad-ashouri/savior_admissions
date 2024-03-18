bfa214b20e5758072bff7cf0aa98171d960b8892
Me Home <exploiter0253@gmail.com>
2024-03-18 22:02:26 +0330

    Temporary comment mobile method for forget password

--- resources/views/Auth/forgot_password.blade.php
+++ resources/views/Auth/forgot_password.blade.php
@@ -25,7 +25,7 @@
                 <label for="reset-options" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select an option</label>
                 <select name="reset-options" id="reset-options" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                     <option selected disabled value="">Choose an option</option>
-                    <option value="Mobile">Mobile</option>
+{{--                    <option value="Mobile">Mobile</option>--}}
                     <option value="Email">Email</option>
                 </select>
             </div>
