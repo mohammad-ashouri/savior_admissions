aa667b941a771dbf264cbc0dd744546475118a48
Me Home <exploiter0253@gmail.com>
2024-03-25 19:11:25 +0330

    Work on SQL data entries

--- database/seeders/DatabaseImportSql.php
+++ database/seeders/DatabaseImportSql.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Catalogs\DocumentType;
+use App\Models\Document;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
@@ -20,5 +22,19 @@ class DatabaseImportSql extends Seeder
         DB::unprepared(file_get_contents('database/migrations/model_has_roles.sql'));
 //        DB::unprepared(file_get_contents('database/migrations/role_has_permissions.sql'));
         DB::unprepared(file_get_contents('database/migrations/documents.sql'));
+
+        //Set all old document type name to document description
+        $allDocuments=Document::get();
+        foreach ($allDocuments as $document){
+
+        }
+
+        //Add new document type for old documents
+        $oldDocumentTypeID=new DocumentType();
+        $oldDocumentTypeID->name='Old Type';
+        $oldDocumentTypeID->status=0;
+        $oldDocumentTypeID->save();
+
+        $resetDocumentsTypeID=Document::query()->update(['document_type_id'=>$oldDocumentTypeID->id]);
     }
 }
