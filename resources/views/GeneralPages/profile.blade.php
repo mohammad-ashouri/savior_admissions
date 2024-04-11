9090026be590c61803e4771c8408c183b54dc446
Me Home <exploiter0253@gmail.com>
2024-04-11 15:04:12 +0330

    Fix complete profile warning

--- resources/views/GeneralPages/profile.blade.php
+++ resources/views/GeneralPages/profile.blade.php
@@ -33,6 +33,24 @@
                     </div>
                 </div>
             @endif
+            @if($myGeneralInformation->status==0)
+                <div
+                    class="bg-yellow-100 border-t-4 border-yellow-500 rounded-b mb-3 text-yellow-900 px-4 py-3 shadow-md"
+                    role="alert">
+                    <div class="flex">
+                        <div class="py-1">
+                            <svg class="fill-current h-6 w-6 text-yellow-500 mr-4" xmlns="http://www.w3.org/2000/svg"
+                                 viewBox="0 0 20 20">
+                                <path
+                                    d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zm12.73-1.41A8 8 0 1 0 4.34 4.34a8 8 0 0 0 11.32 11.32zM9 11V9h2v6H9v-4zm0-6h2v2H9V5z"/>
+                            </svg>
+                        </div>
+                        <div>
+                            <p class="font-bold">Please complete your profile to continue.</p>
+                        </div>
+                    </div>
+                </div>
+            @endif
             <div class="grid grid-cols-3 gap-4 mb-4">
                 <div class="lg:col-span-1 col-span-3 bg-white dark:bg-gray-800 dark:text-white p-8 rounded-lg">
                     <div>
@@ -67,7 +85,8 @@
                                     <label for="first_name_en"
                                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">First
                                         name (English)</label>
-                                    <input type="text" id="first_name_en" value="{{ $myGeneralInformation->first_name_en }}"
+                                    <input type="text" id="first_name_en"
+                                           value="{{ $myGeneralInformation->first_name_en }}"
                                            disabled
                                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                            placeholder="John" required>
@@ -76,7 +95,8 @@
                                     <label for="last_name_en"
                                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Last
                                         name (English)</label>
-                                    <input type="text" id="last_name_en" value="{{ $myGeneralInformation->last_name_en }}"
+                                    <input type="text" id="last_name_en"
+                                           value="{{ $myGeneralInformation->last_name_en }}"
                                            disabled
                                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                            placeholder="Doe" required>
@@ -85,7 +105,8 @@
                                     <label for="first_name_fa"
                                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">First
                                         name (Persian)</label>
-                                    <input type="text" id="first_name_fa" value="{{ $myGeneralInformation->first_name_fa }}"
+                                    <input type="text" id="first_name_fa"
+                                           value="{{ $myGeneralInformation->first_name_fa }}"
                                            disabled
                                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                            required>
@@ -94,7 +115,8 @@
                                     <label for="last_name_fa"
                                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Last
                                         name (Persian)</label>
-                                    <input type="text" id="last_name_fa" value="{{ $myGeneralInformation->last_name_fa }}"
+                                    <input type="text" id="last_name_fa"
+                                           value="{{ $myGeneralInformation->last_name_fa }}"
                                            disabled
                                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                            required>
@@ -139,7 +161,7 @@
                                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nationality</label>
                                     <select required id="nationality"
                                             name="nationality"
-                                            class="select2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                            class=" bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                     >
                                         <option selected disabled value=""></option>
                                         @foreach($countries as $nationality)
@@ -153,7 +175,7 @@
                                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Birthplace</label>
                                     <select required id="birthplace"
                                             name="birthplace"
-                                            class="select2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                            class=" bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                     >
                                         <option selected disabled value=""></option>
                                         @foreach($countries as $birthplaces)
@@ -185,7 +207,7 @@
                                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Country</label>
                                     <select required id="Country"
                                             name="Country"
-                                            class="select2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                            class=" bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                     >
                                         <option selected disabled value=""></option>
                                         @foreach($countries as $country)
