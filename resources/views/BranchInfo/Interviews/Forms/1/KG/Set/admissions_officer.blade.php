e00eb9510dc052fbcf28b71b1599472c0252df92
Me Home <exploiter0253@gmail.com>
2024-04-20 15:43:32 +0330

    Add hidden values to interview forms

--- resources/views/BranchInfo/Interviews/Forms/1/KG/Set/admissions_officer.blade.php
+++ resources/views/BranchInfo/Interviews/Forms/1/KG/Set/admissions_officer.blade.php
@@ -342,6 +342,8 @@
                         <div>
                             <input type="hidden" name="application_id" id="application_id"
                                    value="{{ $interview->id }}">
+                            <input type="hidden" name="form_type" id="form_type"
+                                   value="kga">
                             <button type="submit"
                                     class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                                 Submit
