80361a38603eb354ecbc7baf28e6dcaa76a55530
Me Home <exploiter0253@gmail.com>
2024-04-25 17:38:57 +0330

    Continue if payment is not accepted

--- resources/views/BranchInfo/Interviews/index.blade.php
+++ resources/views/BranchInfo/Interviews/index.blade.php
@@ -94,6 +94,7 @@
 
                             <tbody>
                             @foreach($interviews as $interview)
+                                @if($interview->reservationInfo->payment_status!==1) @continue @endif
                                 <tr
                                     class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
                                     <td class="w-4 p-4">
