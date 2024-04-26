74779b593102425e20a8e72d3031b142d99d8dac
Me Home <exploiter0253@gmail.com>
2024-04-26 20:06:34 +0330

    Fix showing interview form submitted buttons

--- resources/views/BranchInfo/Interviews/index.blade.php
+++ resources/views/BranchInfo/Interviews/index.blade.php
@@ -236,6 +236,16 @@
                                                                     </a>
                                                                 @endcan
                                                             @endif
+                                                            @if(($me->hasRole('Interviewer') and $interview->interview->interviewer==$me->id) or ($me->hasRole('Super Admin') or $me->hasRole('Principal')))
+                                                                @can('interview-show')
+                                                                    <a href="/Interviews/{{ $interview->id }}"
+                                                                       type="button"
+                                                                       class="min-w-max inline-flex font-medium text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300  rounded-lg text-sm px-3 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 focus:outline-none dark:focus:ring-green-800 hover:underline">
+                                                                        <i class="las la-eye mt-1 mr-1"></i>
+                                                                        Show
+                                                                    </a>
+                                                                @endcan
+                                                            @endif
                                                             @break
                                                     @endswitch
                                                     @break
