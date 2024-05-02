c5296914ef3431205c028e086006980a559cf9c7
Me Home <exploiter0253@gmail.com>
2024-05-02 19:39:22 +0330

    Make seeder for change users mobile format to international

--- database/seeders/ChangeUsersMobileFormatToInternational.php
+++ database/seeders/ChangeUsersMobileFormatToInternational.php
@@ -13,14 +13,17 @@ class ChangeUsersMobileFormatToInternational extends Seeder
      */
     public function run(): void
     {
-        User::where('mobile', 'REGEXP', '[^0-9]')
-        ->orWhere('mobile', 'LIKE', '09%')
-            ->orWhere('mobile', 'LIKE', '0%')
-            ->whereHas('roles', function($query) {
+        User::where(function ($query) {
+            $query->where('mobile', 'REGEXP', '[^0-9]') // حذف کاراکترهای غیر عددی
+                ->orWhere('mobile', 'LIKE', '09%')
+                ->orWhere('mobile', 'LIKE', '0%');
+        })
+            ->whereHas('roles', function ($query) {
                 $query->where('name', 'Student')->orWhere('name', 'Parent');
             })
             ->update([
-                'mobile' => DB::raw("CASE WHEN LEFT(mobile, 2) = '09' THEN CONCAT('+98', SUBSTRING(mobile, 3)) ELSE CONCAT('+', mobile) END")
+                'mobile' => DB::raw("CASE WHEN LEFT(mobile, 2) = '09' THEN CONCAT('+98', SUBSTRING(mobile, 3)) ELSE CONCAT('+', mobile) END"),
             ]);
+
     }
 }
