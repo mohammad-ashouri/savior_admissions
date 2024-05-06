9d77e9eae9dd634e823a8eb3e4fc6251736a5667
Me Home <exploiter0253@gmail.com>
2024-05-06 19:42:43 +0330

    Fixed command for destroy expired invoices

---
+++ app/Console/Commands/RemoveExpiredInvoices.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Finance\TuitionInvoiceDetails;
+use App\Models\Finance\TuitionInvoices;
+use App\Models\Invoice;
+use Carbon\Carbon;
+use Illuminate\Console\Command;
+
+class RemoveExpiredInvoices extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:remove-expired-invoices';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'This command removed expired invoices that not paid after 10 minutes';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $tenMinutesAgo = Carbon::now()->subMinutes(12);
+
+        $tuitionInvoiceDetails = TuitionInvoiceDetails::where('created_at', '<=', $tenMinutesAgo)->where('is_paid',0)->where('payment_method',2)->get();
+        foreach ($tuitionInvoiceDetails as $detail) {
+            Invoice::whereJsonContains('description->invoice_details_id',$detail->id)->delete();
+            TuitionInvoices::destroy($detail->tuition_invoice_id);
+        }
+        TuitionInvoiceDetails::where('created_at', '<=', $tenMinutesAgo)->where('is_paid',0)->where('payment_method',2)->delete();
+    }
+}
