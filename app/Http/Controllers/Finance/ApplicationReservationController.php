8b78359c9c9a51d42c30bfdbb99499db047e8e6a
Me Home <exploiter0253@gmail.com>
2024-05-07 23:24:02 +0330

    Fix Reject user from application

--- app/Http/Controllers/Finance/ApplicationReservationController.php
+++ app/Http/Controllers/Finance/ApplicationReservationController.php
@@ -266,7 +266,7 @@ class ApplicationReservationController extends Controller
             $reservatoreMobile=$applicationReservation->reservatore;
             $this->sendSMS($reservatoreMobile, $message);
         }elseif ($applicationStatus == 3){
-            $applianceStatus = StudentApplianceStatus::where('student_id', $applicationReservation->student_id)->where('academic_year', $applicationReservation->applicationInfo->applicationTimingInfo->academic_year)->first();
+            $applianceStatus = StudentApplianceStatus::where('student_id', $applicationReservation->student_id)->where('academic_year', $applicationReservation->applicationInfo->applicationTimingInfo->academic_year)->latest()->first();
             $applianceStatus->interview_status = null;
             $applianceStatus->save();
 
