94634adec5648a5c072be666105dde88315afff2
Me Home <exploiter0253@gmail.com>
2024-05-08 20:54:44 +0330

    Change label of payment type and payment method

--- resources/views/Finance/Tuition/Pay/index.blade.php
+++ resources/views/Finance/Tuition/Pay/index.blade.php
@@ -93,7 +93,7 @@
                                     <div>
                                         <label for="payment_type"
                                                class="block mb-2  font-bold text-gray-900 dark:text-white">
-                                            Select Payment Type</label>
+                                            Payment Type</label>
                                         <select id="payment_type" name="payment_type"
                                                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                                 title="Select student" required>
@@ -107,7 +107,7 @@
                                     <div>
                                         <label for="payment_method"
                                                class="block mb-2  font-bold text-gray-900 dark:text-white">
-                                            Select Payment Method</label>
+                                            Payment Method</label>
                                         <select id="payment_method" name="payment_method"
                                                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                                 title="Select student" required>
