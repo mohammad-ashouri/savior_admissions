8b9c55c0ba667a1103cbc17aace57726e741b346
Me Home <exploiter0253@gmail.com>
2024-05-10 13:52:13 +0330

    Change ordering application reservation by payment status

--- app/Http/Controllers/Finance/ApplicationReservationController.php
+++ app/Http/Controllers/Finance/ApplicationReservationController.php
@@ -70,7 +70,7 @@ class ApplicationReservationController extends Controller
                 ->join('applications', 'application_reservations.application_id', '=', 'applications.id')
                 ->join('application_timings', 'applications.application_timing_id', '=', 'application_timings.id')
                 ->select('application_reservations.*', 'application_reservations.id as application_reservations_id')
-                ->orderBy('application_reservations.payment_status', 'desc')
+                ->orderBy('application_reservations.payment_status', 'asc')
                 ->orderBy('application_timings.academic_year', 'desc')
                 ->paginate(30);
         }
