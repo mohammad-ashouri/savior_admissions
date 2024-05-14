91e701bcf53be86cb7e7213e82cad72a0bd5c778
Me Home <exploiter0253@gmail.com>
2024-05-14 18:53:38 +0330

    Fix all evidences for confirm table style + Add guardian info in columns

--- resources/views/BranchInfo/Evidence/index.blade.php
+++ resources/views/BranchInfo/Evidence/index.blade.php
@@ -54,16 +54,22 @@
                                         #
                                     </div>
                                 </th>
-                                <th scope="col" class="px-6 py-3 text-center">
+                                <th scope="col" class=" text-center">
                                     Academic Year
                                 </th>
-                                <th scope="col" class="px-6 py-3 text-center">
+                                <th scope="col" class=" text-center">
+                                    Student ID
+                                </th>
+                                <th scope="col" class=" text-center">
                                     Student
                                 </th>
-                                <th scope="col" class="px-6 py-3 text-center">
+                                <th scope="col" class=" text-center">
+                                    ID+Guardian
+                                </th>
+                                <th scope="col" class=" text-center">
                                     Level
                                 </th>
-                                <th scope="col" class="px-6 py-3 text-center">
+                                <th scope="col" class=" text-center">
                                     Evidence Form
                                 </th>
                             </tr>
@@ -76,33 +82,35 @@
                                 @endphp
                                 <tr
                                     class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                                    <td class="w-4 p-4">
-                                        <div class="flex items-center">
+                                    <td class="w-4 p-4 text-center">
                                             {{ $loop->iteration }}
-                                        </div>
                                     </td>
                                     <th scope="row"
-                                        class=" items-center text-center px-6 py-4 text-gray-900 whitespace-nowrap dark:text-white">
-                                        <div class="pl-3">
+                                        class=" items-center text-center border text-gray-900 whitespace-nowrap dark:text-white">
                                             <div
                                                 class="text-base font-semibold">{{ $appliance->academicYearInfo->name }}</div>
-                                        </div>
                                     </th>
                                     <th scope="row"
-                                        class=" items-center text-center px-6 py-4 text-gray-900 whitespace-nowrap dark:text-white">
-                                        <div class="pl-3">
+                                        class=" items-center text-center border text-gray-900 whitespace-nowrap dark:text-white">
+                                            <div
+                                                class="text-base font-semibold">{{ $appliance->student_id }}</div>
+                                    </th>
+                                    <th scope="row"
+                                        class=" items-center text-center border text-gray-900 whitespace-nowrap dark:text-white">
                                             <div
                                                 class="text-base font-semibold">{{ $appliance->studentInfo->generalInformationInfo->first_name_en }} {{ $appliance->studentInfo->generalInformationInfo->last_name_en }}</div>
-                                        </div>
                                     </th>
                                     <th scope="row"
-                                        class=" items-center text-center px-6 py-4 text-gray-900 whitespace-nowrap dark:text-white">
-                                        <div class="pl-3">
+                                        class=" items-center text-center border text-gray-900 whitespace-nowrap dark:text-white">
+                                            <div
+                                                class="text-base font-semibold">{{ $appliance->studentInformations->guardian }} - {{ $appliance->studentInformations->guardianInfo->generalInformationInfo->first_name_en }} {{ $appliance->studentInformations->guardianInfo->generalInformationInfo->last_name_en }} </div>
+                                    </th>
+                                    <th scope="row"
+                                        class=" items-center text-center border text-gray-900 whitespace-nowrap dark:text-white">
                                             <div
                                                 class="text-base font-semibold">{{ $applicationReservation->levelInfo->name }}</div>
-                                        </div>
                                     </th>
-                                    <td class="px-6 py-4 text-center">
+                                    <td class="text-center border">
                                         <a href="/ConfirmEvidences/{{$appliance->id}}"
                                            type="button"
                                            class="min-w-max inline-flex font-medium text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300  rounded-lg text-sm px-3 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 focus:outline-none dark:focus:ring-green-800 hover:underline">
