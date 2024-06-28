781e35c46004ac30d6242ec51a07d4aed689fbbb
attaxmoon <m.ashouri.wdev@gmail.com>
2024-06-28 17:36:06 +0330

    Add telescope in register provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        if ($this->app->environment('local','production')) {
+            $this->app->register(\Laravel\Telescope\TelescopeServiceProvider::class);
+            $this->app->register(TelescopeServiceProvider::class);
+        }
     }
 
     /**
