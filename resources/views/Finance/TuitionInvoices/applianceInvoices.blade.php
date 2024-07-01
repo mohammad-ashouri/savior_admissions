8faf28d65a58be6a7b8c015fbc213cad4df4c0fd
attaxmoon <m.ashouri.wdev@gmail.com>
2024-07-01 18:51:36 +0330

    Fix show appliance invoices button link for financial manager

--- resources/views/Finance/TuitionInvoices/applianceInvoices.blade.php
+++ resources/views/Finance/TuitionInvoices/applianceInvoices.blade.php
@@ -228,7 +228,7 @@
                                                 @break
                                             @case('2')
                                                 @if($me->hasRole('Financial Manager') or $me->hasRole('Principal') or $me->hasRole('Super Admin')) @endif
-                                                <a href="/TuitionInvoices/{{ $invoice->tuition_invoice_id }}">
+                                                <a href="/TuitionInvoices/{{ $invoice->id }}">
                                                     <button type="button"
                                                             class="flex text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-3 py-2 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">
                                                         <div class="text-center">
