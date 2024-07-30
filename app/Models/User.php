eae5d0228e233ff9bee1c0047783c8dc29d3188e
attaxmoon <m.ashouri.wdev@gmail.com>
2024-07-30 23:27:00 +0330

    Add softDeletes to user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Lab404\Impersonate\Models\Impersonate;
@@ -13,7 +14,7 @@ use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable, hasPermissions, hasRoles, Impersonate;
+    use HasApiTokens, HasFactory, Notifiable, hasPermissions, hasRoles, Impersonate, SoftDeletes;
 
     /**
      * The attributes that are mass assignable.
