9e82a1da2aa897520db272411c71da8509360404
attaxmoon <m.ashouri.wdev@gmail.com>
2025-01-06 21:38:55 +0330

    Fix showing application information where payment status is 1

--- resources/views/GeneralPages/PDF/TuitionCard/2024/tuition_card_en.blade.php
+++ resources/views/GeneralPages/PDF/TuitionCard/2024/tuition_card_en.blade.php
@@ -8,6 +8,7 @@
     $applicationInformation=ApplicationTiming::join('applications','application_timings.id','=','applications.application_timing_id')
                                                 ->join('application_reservations','applications.id','=','application_reservations.application_id')
                                                 ->where('application_reservations.student_id',$applianceStatus->student_id)
+                                                ->where('application_reservations.payment_status',1)
                                                 ->where('application_timings.academic_year',$applianceStatus->academic_year)
                                                 ->where('application_reservations.deleted_at',null)
                                                 ->latest('application_reservations.id')
