dda498ad8bb92e44c886e672b2b1bad5b2b86cc7
SH <m.ashouri.wdev@gmail.com>
2025-01-06 10:08:25 +0330

    Fix showing null guardian in student status

--- resources/views/BranchInfo/StudentStatuses/index.blade.php
+++ resources/views/BranchInfo/StudentStatuses/index.blade.php
@@ -235,7 +235,7 @@
                                     <td class="border text-center">
                                         @if($guardian)
                                             <button type="button" data-id="{{ $guardian->mobile }}"
-                                                    data-info="{{ $guardian->generalInformationInfo->first_name_en }} {{ $guardian->generalInformationInfo->last_name_en }}"
+                                                    data-info="{{ $guardian->generalInformationInfo?->first_name_en }} {{ $guardian->generalInformationInfo?->last_name_en }}"
                                                     class="show-guardian-mobile text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-3 py-1 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                                                 <i class="las la-phone "></i>
                                             </button>
