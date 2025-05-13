e2602765cb3689b99b0b4663f4aaf8a01e603bfa
SH <m.ashouri.wdev@gmail.com>
2025-05-13 08:47:02 +0330

    Reset email method from forgot password

--- resources/views/Auth/ForgotPassword/reset_password.blade.php
+++ resources/views/Auth/ForgotPassword/reset_password.blade.php
@@ -29,12 +29,7 @@
         </h2>
         <form id="reset-password" class="space-y-4 w-full">
             <div class="mb-6">
-                @if($tokenInfo->type==1)
-                    <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your
-                        email</label>
-                    <input type="email" id="email" name="email" value="{{ $userInfo->email }}" disabled
-                           class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                @elseif($tokenInfo->type==2)
+                @if($tokenInfo->type==2)
                     <label for="mobile" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your
                         mobile</label>
                     <input type="text" id="mobile" name="mobile" value="{{ $userInfo->mobile }}" disabled
