061423ef6355f572e5e7e0b119b4247a4eef72bd
SH <m.ashouri.wdev@gmail.com>
2025-05-28 11:30:49 +0330

    Work on file upload bug

--- config/livewire.php
+++ config/livewire.php
@@ -67,7 +67,7 @@ return [
         'disk' => 'local',        // Example: 'local', 's3'              | Default: 'default'
         'rules' => ['file', 'mimes:png,jpg', 'max:12288'],       // Example: ['file', 'mimes:png,jpg']  | Default: ['required', 'file', 'max:12288'] (12MB)
         'directory' => 'livewire-tmp',   // Example: 'tmp'                      | Default: 'livewire-tmp'
-        'middleware' => null,  // Example: 'throttle:5,1'             | Default: 'throttle:60,1'
+        'middleware' => 'web',  // Example: 'throttle:5,1'             | Default: 'throttle:60,1'
         'preview_mimes' => [   // Supported file types for temporary pre-signed file URLs...
             'png', 'gif', 'bmp', 'svg', 'wav', 'mp4',
             'mov', 'avi', 'wmv', 'mp3', 'm4a',
