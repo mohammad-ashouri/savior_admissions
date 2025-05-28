ca93b769704827eeed81405e1d9c977e3ba509e8
SH <m.ashouri.wdev@gmail.com>
2025-05-28 10:35:53 +0330

    Work on file upload bug

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -15,5 +15,6 @@ class VerifyCsrfToken extends Middleware
         '/VerifyApplicationPayment',
         '/VerifyTuitionPayment',
         '/VerifyTuitionInstallmentPayment',
+        '/livewire/*',
     ];
 }
